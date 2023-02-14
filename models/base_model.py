f3ff2b8a644ffe4d7f96bb2b95a58fc151089bf0
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-02-14 14:44:33 +0000

    Add a  method to base_model

--- models/base_model.py
+++ models/base_model.py
@@ -48,3 +48,14 @@ class BaseModel:
         my_dict["created_at"] = my_dict["created_at"].isoformat()
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
         return my_dict
+    
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+
+        return my_dict
