ed9c740c004195917bb5dac6ee663a44b8b7164a
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-02-15 15:42:38 +0000

    Fix failing save() error

--- models/base_model.py
+++ models/base_model.py
@@ -42,7 +42,7 @@ class BaseModel:
 
     def save(self):
         """Updates the public instance attribute 'updated_at'"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         storage.save()
 
     def to_dict(self):
